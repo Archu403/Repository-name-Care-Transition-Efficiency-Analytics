cf37840cffa2b4a7119424402be3b17cb547928d
Archu403 <archuarchu403@gmail.com>
2026-06-03 11:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,18 @@ st.dataframe(df.head())
 st.sidebar.header(" Filters")
 
 column = st.sidebar.selectbox("Choose column to explore", df.columns)
-
 unique_values = df[column].dropna().unique()
 selected_value = st.sidebar.selectbox("Select value", unique_values)
-
 filtered_df = df[df[column] == selected_value]
 
 st.write("### Filtered Data")
 st.dataframe(filtered_df)
 import plotly.express as px
-
 st.write(" Basic Visualization")
-
 numeric_cols = df.select_dtypes(include="number").columns
 
 if len(numeric_cols) > 0:
     col = st.selectbox("Choose numeric column for chart", numeric_cols)
-
     fig = px.histogram(df, x=col, title=f"Distribution of {col}")
     st.plotly_chart(fig)
 else:
